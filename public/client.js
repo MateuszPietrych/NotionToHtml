0f5a0651531ec16af9dde0c74557bcafb1be793e
MateuszPietrych <mateusz.pietrych@gmail.com>
2024-07-23 20:10:15 +0200

    Add first version of code

---
+++ public/client.js
@@ -0,0 +1,193 @@
+// This file is run by the browser each time your view template is loaded
+
+/**
+ * Define variables that reference elements included in /views/index.html:
+ */
+
+// Forms
+// const dbForm = document.getElementById("databaseForm")
+// const pageForm = document.getElementById("pageForm")
+// const blocksForm = document.getElementById("blocksForm")
+// const commentForm = document.getElementById("commentForm")
+const getPageForm = document.getElementById("getPageForm")
+
+// Table cells where API responses will be appended
+const dbResponseEl = document.getElementById("dbResponse")
+const pageResponseEl = document.getElementById("pageResponse")
+const blocksResponseEl = document.getElementById("blocksResponse")
+const commentResponseEl = document.getElementById("commentResponse")
+
+/**
+ * Functions to handle appending new content to /views/index.html
+ */
+
+// Appends the API response to the UI
+
+
+const appendApiResponse = function (apiResponse, el) {
+  console.log(apiResponse)
+
+  // Add success message to UI
+  const newParagraphSuccessMsg = document.createElement("p")
+  newParagraphSuccessMsg.textContent = "Result: " + apiResponse.message
+  el.appendChild(newParagraphSuccessMsg)
+  // See browser console for more information
+  if (apiResponse.message === "error") return
+
+  // Add ID of Notion item (db, page, comment) to UI
+  const newParagraphId = document.createElement("p")
+  newParagraphId.textContent = "ID: " + apiResponse.data.id
+  el.appendChild(newParagraphId)
+
+  // Add URL of Notion item (db, page) to UI
+  if (apiResponse.data.url) {
+    const newAnchorTag = document.createElement("a")
+    newAnchorTag.setAttribute("href", apiResponse.data.url)
+    newAnchorTag.innerText = apiResponse.data.url
+    el.appendChild(newAnchorTag)
+  }
+}
+
+// Appends the blocks API response to the UI
+const appendBlocksResponse = function (apiResponse, el) {
+  console.log(apiResponse)
+
+  // Add success message to UI
+  const newParagraphSuccessMsg = document.createElement("p")
+  newParagraphSuccessMsg.textContent = "Result: " + apiResponse.message
+  el.appendChild(newParagraphSuccessMsg)
+
+  // Add block ID to UI
+  const newParagraphId = document.createElement("p")
+  newParagraphId.textContent = "ID: " + apiResponse.data.results[0].id
+  el.appendChild(newParagraphId)
+}
+
+/**
+ * Attach submit event handlers to each form included in /views/index.html
+ */
+
+
+getPageForm.onsubmit = async function (event) {
+  event.preventDefault()
+
+  document.getElementById("test").innerText = "XDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDddd";
+  const dbName = event.target.dbName.value
+  const body = JSON.stringify({ dbName })
+
+  const newDBResponse = await fetch("/getPage", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body,
+  })
+  console.log("1.")
+  const newDBData = await newDBResponse.json()
+  console.log("2.")
+  console.log(newDBData.data);
+  console.log("3.")
+  // const prepareTexts = prepareTexts(newDBData.data)
+
+  makeHtmlFromTexts(newDBData.data)
+
+  // document.getElementById("test").innerText = newDBData.data;
+  
+
+  // appendApiResponse(newDBData, dbResponseEl)
+}
+
+
+
+function makeHtmlFromTexts(texts) {
+
+  const mainDiv = document.getElementById("test")
+  textBlocks = []
+  for (let i = 0; i < texts.length; i++) {
+    const newParagraphSuccessMsg = document.createElement(texts[i][0])
+    newParagraphSuccessMsg.textContent = texts[i][1]
+    mainDiv.appendChild(newParagraphSuccessMsg)
+  }
+  
+  return textBlocks
+}
+
+
+
+
+// // Attach submit event to each form
+// dbForm.onsubmit = async function (event) {
+//   event.preventDefault()
+
+//   const dbName = event.target.dbName.value
+//   const body = JSON.stringify({ dbName })
+
+//   const newDBResponse = await fetch("/databases", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body,
+//   })
+//   const newDBData = await newDBResponse.json()
+
+//   appendApiResponse(newDBData, dbResponseEl)
+// }
+
+// pageForm.onsubmit = async function (event) {
+//   event.preventDefault()
+
+//   const dbID = event.target.newPageDB.value
+//   const pageName = event.target.newPageName.value
+//   const header = event.target.header.value
+//   const body = JSON.stringify({ dbID, pageName, header })
+
+//   const newPageResponse = await fetch("/pages", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body,
+//   })
+
+//   const newPageData = await newPageResponse.json()
+//   appendApiResponse(newPageData, pageResponseEl)
+// }
+
+// blocksForm.onsubmit = async function (event) {
+//   event.preventDefault()
+
+//   const pageID = event.target.pageID.value
+//   const content = event.target.content.value
+//   const body = JSON.stringify({ pageID, content })
+
+//   const newBlockResponse = await fetch("/blocks", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body,
+//   })
+
+//   const newBlockData = await newBlockResponse.json()
+//   appendBlocksResponse(newBlockData, blocksResponseEl)
+// }
+
+// commentForm.onsubmit = async function (event) {
+//   event.preventDefault()
+
+//   const pageID = event.target.pageIDComment.value
+//   const comment = event.target.comment.value
+//   const body = JSON.stringify({ pageID, comment })
+
+//   const newCommentResponse = await fetch("/comments", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body,
+//   })
+
+//   const newCommentData = await newCommentResponse.json()
+//   appendApiResponse(newCommentData, commentResponseEl)
+// }
---
+++ server.js
@@ -0,0 +1,398 @@
+require("dotenv").config()
+const express = require("express")
+const app = express()
+
+const { Client } = require("@notionhq/client")
+const notion = new Client({ auth: process.env.NOTION_KEY })
+
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"))
+app.use(express.json()) // for parsing application/json
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + "/views/index.html")
+})
+
+
+
+app.post("/getPage",  async function (request, response) {
+
+  const blockId = request.body.dbName;
+  const requestResponse = await notion.blocks.children.list({
+    block_id: blockId,
+    page_size: 50,
+  });
+  //console.log(requestResponse);
+  //printBlockText(requestResponse.results)
+  const changeBlocksForTextList = changeBlocksForText(requestResponse.results)
+
+  changeBlocksForTextList.then(
+    function(value) { response.json({message: "success!", data:  value})},
+    function(error) { response.json({message: "fail!", data:  error}) }
+  );
+}) 
+
+async function getPageById(id) {
+  console.log("Retrieving page (async)...")
+  
+  const blockId = id;
+  const requestResponse = await notion.blocks.children.list({
+    block_id: blockId,
+    page_size: 50,
+  });
+
+  return requestResponse.results
+}
+
+
+// app.post("/getPage",  async function (request, response) {
+
+//   const blockId = request.body.dbName;
+//   const requestResponse = await notion.blocks.retrieve({
+//     block_id: blockId,
+//   });
+//   console.log(requestResponse);
+
+//   response.json({message: "success!", data: requestResponse })
+// })
+const getPlainTextFromRichText = richText => {
+  return richText.map(t => t.plain_text).join("")
+  // Note: A page mention will return "Undefined" as the page name if the page has not been shared with the integration. See: https://developers.notion.com/reference/block#mention
+}
+
+async function getTextFromBlock (block){
+  let text
+
+  // Get rich text from blocks that support it
+  
+  hasRichText = false;
+  try{
+    block[block.type].rich_text;
+    hasRichText = true;
+  }
+  catch(e){
+    hasRichText = false;
+  }
+
+  if (hasRichText && block[block.type].rich_text) {
+    // This will be an empty string if it's an empty line.
+    text = getPlainTextFromRichText(block[block.type].rich_text)
+  }
+  // Get text for block types that don't have rich text
+  else {
+    switch (block.type) {
+      case "unsupported":
+        // The public API does not support all block types yet
+        text = "[Unsupported block type]"
+        break
+      case "bookmark":
+        text = block.bookmark.url
+        break
+      case "child_database":
+        text = block.child_database.title
+        // Use "Query a database" endpoint to get db rows: https://developers.notion.com/reference/post-database-query
+        // Use "Retrieve a database" endpoint to get additional properties: https://developers.notion.com/reference/retrieve-a-database
+        break
+      case "child_page":
+        const page = await getPageById(block.id)
+        const changeBlocksForTextList = await changeBlocksForText(page)
+        return changeBlocksForTextList;
+      case "embed":
+      case "video":
+      case "file":
+      case "image":
+      case "pdf":
+        //text = getMediaSourceText(block)
+        break
+      case "equation":
+        text = block.equation.expression
+        break
+      case "link_preview":
+        text = block.link_preview.url
+        break
+      case "synced_block":
+        // Provides ID for block it's synced with.
+        text = block.synced_block.synced_from
+          ? "This block is synced with a block with the following ID: " +
+            block.synced_block.synced_from[block.synced_block.synced_from.type]
+          : "Source sync block that another blocked is synced with."
+        break
+      case "table":
+        // Only contains table properties.
+        // Fetch children blocks for more details.
+        text = "Table width: " + block.table.table_width
+        break
+      case "table_of_contents":
+        // Does not include text from ToC; just the color
+        text = "ToC color: " + block.table_of_contents.color
+        break
+      case "breadcrumb":
+      case "column_list":
+      case "divider":
+        text = "No text available"
+        break
+      default:
+        console.log(block)
+        text = "[Needs case added]"
+        break
+    }
+  }
+
+  switch(block.type){
+    case "heading_1": return [["h1", text]];
+    case "heading_2": return [["h2", text]];
+    case "heading_3": return [["h3", text]];
+    case "paragraph": return [["p", text]];
+    default: return ["p", text];
+  }
+
+}
+
+async function retrieveBlockChildren(id) {
+  console.log("Retrieving blocks (async)...")
+  const blocks = []
+
+  // Use iteratePaginatedAPI helper function to get all blocks first-level blocks on the page
+  for await (const block of iteratePaginatedAPI(notion.blocks.children.list, {
+    block_id: id, // A page ID can be passed as a block ID: https://developers.notion.com/docs/working-with-page-content#modeling-content-as-blocks
+  })) {
+    blocks.push(block)
+  }
+
+  return blocks
+}
+
+function getBlockById(id) {
+  return notion.blocks.retrieve({ block_id: id })
+}
+
+
+
+// const printBlockText = blocks => {
+//   console.log("Displaying blocks:")
+
+//   for (let i = 0; i < blocks.length; i++) {
+//     const text = getTextFromBlock(blocks[i])
+//     // Print plain text for each block.
+//     console.log(text)
+//   }
+// }
+
+async function changeBlocksForText(blocks) {
+
+  let textBlocks = []
+  console.log("number of blocks: " + blocks.length)
+  for (let i = 0; i < blocks.length; i++) {
+    //console.log( i + ". BLOCK------------------------------------------------------------")
+    const texts = await getTextFromBlock(blocks[i])
+    // console.log(texts)
+    for(let j = 0; j < texts.length; j++){
+      textBlocks.push(texts[j])
+    }
+    //textBlocks.concat(value)
+    // console.log(texts)
+    // textBlocks = textBlocks.concat(texts)
+    //console.log("-----------\n\n")
+  }
+  console.log(textBlocks)
+  
+  return textBlocks
+}
+
+// function prepareTexts(texts){
+
+//   preparedTextList = []
+//   for(let i = 0; i < texts.length; i++){
+//     const text = texts[i]
+//     const splitIndex =  text.search(":")
+
+//     const splitTextArray = [text.slice(0, splitIndex), text.slice(splitIndex+1)]
+
+//     console.log(splitTextArray)
+//     preparedTextList.push(splitTextArray)
+//   }
+//   return preparedTextList
+// }
+
+
+// function makeHtmlFromTexts(texts) {
+
+//   const mainDiv = document.getElementById("test")
+//   textBlocks = []
+//   for (let i = 0; i < blocks.length; i++) {
+//     const text = getTextFromBlock(blocks[i])
+//     textBlocks.push(text)
+//     // Print plain text for each block.
+
+
+//     const newParagraphSuccessMsg = document.createElement("p")
+//     newParagraphSuccessMsg.textContent = text
+//     mainDiv.appendChild(newParagraphSuccessMsg)
+//   }
+  
+//   return textBlocks
+// }
+
+function chooseElementFromText(text){
+  switch(text){
+    case "heading_1": return "h1";
+    case "heading_2": return "h2";
+    case "heading_3": return "h3";
+    case "paragraph": return "p";
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Create new database. The page ID is set in the environment variables.
+app.post("/databases", async function (request, response) {
+  const pageId = process.env.NOTION_PAGE_ID
+  const title = request.body.dbName
+
+  console.log("NOTION_PAGE_ID: %s", pageId)
+  console.log("NOTION_KEY: %s", process.env.NOTION_KEY)
+
+  try {
+    const newDb = await notion.databases.create({
+      parent: {
+        type: "page_id",
+        page_id: pageId,
+      },
+      title: [
+        {
+          type: "text",
+          text: {
+            content: title,
+          },
+        },
+      ],
+      properties: {
+        Name: {
+          title: {},
+        },
+      },
+    })
+    response.json({ message: "success!", data: newDb })
+  } catch (error) {
+    response.json({ message: "error", error })
+  }
+})
+
+// Create new page. The database ID is provided in the web form.
+app.post("/pages", async function (request, response) {
+  const { dbID, pageName, header } = request.body
+
+  try {
+    const newPage = await notion.pages.create({
+      parent: {
+        type: "database_id",
+        database_id: dbID,
+      },
+      properties: {
+        Name: {
+          title: [
+            {
+              text: {
+                content: pageName,
+              },
+            },
+          ],
+        },
+      },
+      children: [
+        {
+          object: "block",
+          heading_2: {
+            rich_text: [
+              {
+                text: {
+                  content: header,
+                },
+              },
+            ],
+          },
+        },
+      ],
+    })
+    response.json({ message: "success!", data: newPage })
+  } catch (error) {
+    response.json({ message: "error", error })
+  }
+})
+
+// Create new block (page content). The page ID is provided in the web form.
+app.post("/blocks", async function (request, response) {
+  const { pageID, content } = request.body
+
+  try {
+    const newBlock = await notion.blocks.children.append({
+      block_id: pageID, // a block ID can be a page ID
+      children: [
+        {
+          // Use a paragraph as a default but the form or request can be updated to allow for other block types: https://developers.notion.com/reference/block#keys
+          paragraph: {
+            rich_text: [
+              {
+                text: {
+                  content: content,
+                },
+              },
+            ],
+          },
+        },
+      ],
+    })
+    response.json({ message: "success!", data: newBlock })
+  } catch (error) {
+    response.json({ message: "error", error })
+  }
+})
+
+// Create new page comments. The page ID is provided in the web form.
+app.post("/comments", async function (request, response) {
+  const { pageID, comment } = request.body
+
+  try {
+    const newComment = await notion.comments.create({
+      parent: {
+        page_id: pageID,
+      },
+      rich_text: [
+        {
+          text: {
+            content: comment,
+          },
+        },
+      ],
+    })
+    response.json({ message: "success!", data: newComment })
+  } catch (error) {
+    response.json({ message: "error", error })
+  }
+})
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port)
+})
