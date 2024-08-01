59d1b13805bb94cfa2bfc14e67a7031d5aa9fbe5
MateuszPietrych <mateusz.pietrych@gmail.com>
2024-08-01 03:24:51 +0200

    Clean code, and field for file name

--- public/client.js
+++ public/client.js
@@ -1,103 +1,42 @@
-// This file is run by the browser each time your view template is loaded
-
-/**
- * Define variables that reference elements included in /views/index.html:
- */
-
-// Forms
 
 const getPageForm = document.getElementById("getPageForm")
-
-// Table cells where API responses will be appended
-const dbResponseEl = document.getElementById("dbResponse")
-const pageResponseEl = document.getElementById("pageResponse")
-const blocksResponseEl = document.getElementById("blocksResponse")
-const commentResponseEl = document.getElementById("commentResponse")
-
-
-
-/**
- * Functions to handle appending new content to /views/index.html
- */
-
-// Appends the API response to the UI
-
-
-const appendApiResponse = function (apiResponse, el) {
-  console.log(apiResponse)
-
-  // Add success message to UI
-  const newParagraphSuccessMsg = document.createElement("p")
-  newParagraphSuccessMsg.textContent = "Result: " + apiResponse.message
-  el.appendChild(newParagraphSuccessMsg)
-  // See browser console for more information
-  if (apiResponse.message === "error") return
-
-  // Add ID of Notion item (db, page, comment) to UI
-  const newParagraphId = document.createElement("p")
-  newParagraphId.textContent = "ID: " + apiResponse.data.id
-  el.appendChild(newParagraphId)
-
-  // Add URL of Notion item (db, page) to UI
-  if (apiResponse.data.url) {
-    const newAnchorTag = document.createElement("a")
-    newAnchorTag.setAttribute("href", apiResponse.data.url)
-    newAnchorTag.innerText = apiResponse.data.url
-    el.appendChild(newAnchorTag)
-  }
-}
-
-// Appends the blocks API response to the UI
-const appendBlocksResponse = function (apiResponse, el) {
-  console.log(apiResponse)
-
-  // Add success message to UI
-  const newParagraphSuccessMsg = document.createElement("p")
-  newParagraphSuccessMsg.textContent = "Result: " + apiResponse.message
-  el.appendChild(newParagraphSuccessMsg)
-
-  // Add block ID to UI
-  const newParagraphId = document.createElement("p")
-  newParagraphId.textContent = "ID: " + apiResponse.data.results[0].id
-  el.appendChild(newParagraphId)
-}
-
-/**
- * Attach submit event handlers to each form included in /views/index.html
- */
 
 
 getPageForm.onsubmit = async function (event) {
   event.preventDefault()
 
-  const dbName = event.target.dbName.value
-  const body = JSON.stringify({ dbName })
+  const notionPageId = event.target.notionPageId.value
+  const fileName = event.target.fileName.value
+  
+  await createHtmlFromNotionPage(notionPageId)
 
-  const newDBResponse = await fetch("/getPage", {
+  deleteElement("mainForm")
+
+  await createPdfFileFromtHtml(fileName)
+}
+
+async function createHtmlFromNotionPage(notionPageId) {
+  const body = JSON.stringify({ notionPageId: notionPageId })
+
+  const getPageResponse = await fetch("/getPage", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body,
+    body: body,
   })
-  const newDBData = await newDBResponse.json()
-  console.log(newDBData.data);
+  const getPageResponseData = await getPageResponse.json()
 
-  makeHtmlFromTexts(newDBData.data)
+  makeHtmlFromTexts(getPageResponseData.data)
+}
 
-  var element = document.getElementById('form');
-  if (element) {
-      element.remove();
+async function createPdfFileFromtHtml(fileName, htmlString = null) {
+  if(htmlString == null){
+    htmlString = '<!DOCTYPE html><html lang="en"><head>' + document.head.innerHTML + '</head>' + '<body>' + document.body.innerHTML + '</body>' + '</html>';
   }
+  const data = JSON.stringify({ htmlString, fileName });
 
-  
-  htmlString = '<!DOCTYPE html><html lang="en"><head>'+  document.head.innerHTML + '</head>'+ '<body>'+  document.body.innerHTML + '</body>'+'</html>';
-
-  const data = JSON.stringify({htmlString});
-
-  console.log("Length of string: " +  data.length);
-
-  const pdfResponse = await fetch("/toPdf", {
+  const toPdfResponse = await fetch("/toPdf", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -105,18 +44,22 @@ getPageForm.onsubmit = async function (event) {
     body: data,
   })
 
+  const toPdfResponseData = await toPdfResponse.json()
+  console.log(toPdfResponseData)
+}
+
+function deleteElement(elementName) {
+  var element = document.getElementById(elementName);
+  if (element) {
+    element.remove();
+  }
 }
 
 
-
-
-
-
-function addEnter(){
-    const mainDiv = document.getElementById("main")
-    const newParagraphSuccessMsg = document.createElement("br")
-    mainDiv.appendChild(newParagraphSuccessMsg)
-  
+function addEnter() {
+  const mainDiv = document.getElementById("main")
+  const newParagraphSuccessMsg = document.createElement("br")
+  mainDiv.appendChild(newParagraphSuccessMsg)
 }
 
 function makeHtmlFromTexts(texts) {
@@ -124,104 +67,27 @@ function makeHtmlFromTexts(texts) {
   const mainDiv = document.getElementById("main")
   textBlocks = []
   for (let i = 0; i < texts.length; i++) {
-    if(texts[i]) {
+    if (texts[i]) {
       const newParagraphSuccessMsg = document.createElement(texts[i][0])
-      if(texts[i][0] != "h1") {
+      if (texts[i][0] != "h1") {
         addEnter();
       }
 
-  
-      if(texts[1] != null) { 
-        if(texts[i][0] == "img" && texts[i][1].startsWith("http")) {
-          console.log(texts[i][1])
+      if (texts[1] != null) {
+        if (texts[i][0] == "img" && texts[i][1].startsWith("http")) {
           newParagraphSuccessMsg.src = texts[i][1]
-        }else {
+        } else {
           newParagraphSuccessMsg.textContent = texts[i][1]
         }
       } else {
         newParagraphSuccessMsg.textContent = "\n"
       }
+      newParagraphSuccessMsg.align = "justify"
       mainDiv.appendChild(newParagraphSuccessMsg)
     }
-   
   }
-  
+
   return textBlocks
 }
 
-// // Attach submit event to each form
-// dbForm.onsubmit = async function (event) {
-//   event.preventDefault()
 
-//   const dbName = event.target.dbName.value
-//   const body = JSON.stringify({ dbName })
-
-//   const newDBResponse = await fetch("/databases", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body,
-//   })
-//   const newDBData = await newDBResponse.json()
-
-//   appendApiResponse(newDBData, dbResponseEl)
-// }
-
-// pageForm.onsubmit = async function (event) {
-//   event.preventDefault()
-
-//   const dbID = event.target.newPageDB.value
-//   const pageName = event.target.newPageName.value
-//   const header = event.target.header.value
-//   const body = JSON.stringify({ dbID, pageName, header })
-
-//   const newPageResponse = await fetch("/pages", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body,
-//   })
-
-//   const newPageData = await newPageResponse.json()
-//   appendApiResponse(newPageData, pageResponseEl)
-// }
-
-// blocksForm.onsubmit = async function (event) {
-//   event.preventDefault()
-
-//   const pageID = event.target.pageID.value
-//   const content = event.target.content.value
-//   const body = JSON.stringify({ pageID, content })
-
-//   const newBlockResponse = await fetch("/blocks", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body,
-//   })
-
-//   const newBlockData = await newBlockResponse.json()
-//   appendBlocksResponse(newBlockData, blocksResponseEl)
-// }
-
-// commentForm.onsubmit = async function (event) {
-//   event.preventDefault()
-
-//   const pageID = event.target.pageIDComment.value
-//   const comment = event.target.comment.value
-//   const body = JSON.stringify({ pageID, comment })
-
-//   const newCommentResponse = await fetch("/comments", {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body,
-//   })
-
-//   const newCommentData = await newCommentResponse.json()
-//   appendApiResponse(newCommentData, commentResponseEl)
-// }
--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 require("dotenv").config()
+var fs = require("fs");
+const path = require('path');
 const express = require("express")
 const app = express()
-const { chromium } = require('playwright'); 
-
-
-
+const { chromium } = require('playwright');
 const { Client } = require("@notionhq/client")
 const notion = new Client({ auth: process.env.NOTION_KEY })
 
 
+
+
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"))
-// app.use(express.json()) // for parsing application/json
 
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
@@ -24,62 +24,71 @@ app.get("/", function (request, response) {
 app.post("/toPdf", async function (request, response) {
 
   const html = request.body.htmlString;
-  
-  const browser = await chromium.launch({ headless: true });
+  const fileName = request.body.fileName;
+  const pathToSaves = process.env.DIR_FOR_EXPORT + "/" + fileName + ".pdf";
 
-  const context = await browser.newContext();
+  fs.mkdir(path.join(__dirname, process.env.DIR_FOR_EXPORT),
+    { recursive: true },
+    (err) => {
+        if (err) {
+            return console.error(err);
+        }
+    });
 
-  const page = await context.newPage();
-
-  await page.setContent(html);
-
-  await page.pdf({ path: 'invoice.pdf', format: 'A4', margin: { top: '1.5cm', right: '1.5cm', bottom: '2cm', left: '1.5cm' } });
-
-  // do something ...
-
-  await browser.close();
-  response.json({message: "success!", data: "test"})
+  try 
+  {
+    const browser = await chromium.launch({ headless: true });
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.setContent(html);
+    await page.pdf({ path: pathToSaves, format: 'A4', margin: { top: '1.5cm', right: '1.5cm', bottom: '2cm', left: '1.5cm' }, printBackground: true });
+    await browser.close();
+    response.json({ message: "Success", data: "Html convert to PDF" })
+  }
+  catch (e) {
+    response.json({ message: "Failure", data: e })
+    console.log(e);
+  }
 })
 
-app.post("/getPage",  async function (request, response) {
+app.post("/getPage", async function (request, response) {
 
-  const blockId = request.body.dbName;
+  const blockId = request.body.notionPageId;
 
   const page = await notion.blocks.retrieve({
     block_id: blockId,
-    page_size: 50,
+    page_size: process.env.PAGE_SIZE,
   });
 
   const childrenList = await notion.blocks.children.list({
     block_id: blockId,
-    page_size: 50,
+    page_size: process.env.PAGE_SIZE,
   });
   const changeBlocksForTextList = changeBlocksForText(page, childrenList.results)
- 
 
   changeBlocksForTextList.then(
-    function(value) { response.json({message: "success!", data:  value})},
-    function(error) { response.json({message: "fail!", data:  error}) }
+    function (value) { response.json({ message: "Success", data: value }) },
+    function (error) { response.json({ message: "Failure", data: error }) }
   );
-}) 
+})
 
 async function getPageChildrenById(id) {
-  
+
   const blockId = id;
   const children = await notion.blocks.children.list({
     block_id: blockId,
-    page_size: 50,
+    page_size: process.env.PAGE_SIZE,
   });
 
   return children.results
 }
 
 async function getPageById(id) {
-  
+
   const blockId = id;
   const page = await notion.blocks.retrieve({
     block_id: blockId,
-    page_size: 50,
+    page_size: process.env.PAGE_SIZE,
   });
 
   return page
@@ -92,236 +101,141 @@ async function changeChildrenBlocksForTextListById(id) {
   return changeBlocksForTextList;
 }
 
+async function changeBlocksForText(page, children, type = "page") {
+
+  let textBlocks = []
+
+  switch(type) {
+    case "page":
+      textBlocks = [["h1", page.child_page.title]]
+      break
+    case "toggle":
+      textBlocks = [["h2", page.toggle.rich_text[0].plain_text]]
+      break
+    default:
+      break
+  }
+
+  
+  for (let i = 0; i < children.length; i++) {
+    const texts = await getTextFromBlock(children[i])
+    for (let j = 0; j < texts.length; j++) {
+      textBlocks.push(texts[j])
+    }
+  }
+  return textBlocks
+}
+
 
 const getPlainTextFromRichText = richText => {
   return richText.map(t => t.plain_text).join("")
   // Note: A page mention will return "Undefined" as the page name if the page has not been shared with the integration. See: https://developers.notion.com/reference/block#mention
 }
 
-async function getTextFromBlock (block){
+async function getTextFromBlock(block) {
   let text
 
-  // Get rich text from blocks that support it
   
 
+  if(block.type == "child_page") {
+    return await changeChildrenBlocksForTextListById(block.id);
+  }
+  else if (block.type == "toggle" || block.type == "column_list" || block.type == "column") {
+    const page = await getPageById(block.id);
+    const childrenBlockTexts = await getPageChildrenById(block.id);
+    const changeBlocksForTextList = await changeBlocksForText(page, childrenBlockTexts, block.type );
+    return changeBlocksForTextList;
+  } 
+  
+
+  // Check if block has rich text
   hasRichText = false;
-  try{
+  try {
     block[block.type].rich_text;
     hasRichText = true;
   }
-  catch(e){
+  catch (e) {
     hasRichText = false;
   }
 
-  // console.log("\n\nblock: " + JSON.stringify(block))
-  
 
   if (hasRichText && block[block.type].rich_text) {
-    if(block.type == "toggle"){
-      // console.log("\n\nblock: " + JSON.stringify(block))
-      const page = await getPageById(block.id);
-      const childrenBlockTexts = await getPageChildrenById(block.id);
-      const changeBlocksForTextList = await changeBlocksForText(page, childrenBlockTexts, true);
-      return changeBlocksForTextList;
-    }
-    // This will be an empty string if it's an empty line.
-    text = getPlainTextFromRichText(block[block.type].rich_text)
-    // console.log("Block type: " + block.type + "  -  " + text)
-
+    text = await getTextFromRichTextBlock(block);
   }
-  // Get text for block types that don't have rich text
   else {
-
-    switch (block.type) {
-      case "unsupported":
-        // The public API does not support all block types yet
-        text = "[Unsupported block type]"
-        break
-      case "paragraph":
-        text = block.paragraph.text
-        break
-      // case "bookmark":
-      //   text = block.bookmark.url
-      //   break
-      case "child_database":
-        //text = block.child_database.title
-        text = "child_database ----------------------------------------------"
-
-        // Use "Query a database" endpoint to get db rows: https://developers.notion.com/reference/post-database-query
-        // Use "Retrieve a database" endpoint to get additional properties: https://developers.notion.com/reference/retrieve-a-database
-        break
-      case "child_page":
-        return await changeChildrenBlocksForTextListById(block.id);
-      // case "embed":
-      // case "video":
-      // case "file":
-      case "image":
-        // text = block.image.file
-        const jsonString = JSON.stringify(block);
-        const urlMatch = jsonString.match(/"url":"(.*?)"/);
-
-        if (urlMatch && urlMatch[1]) {
-          const url = urlMatch[1];
-          text = url;
-        } else {
-          console.log('URL not found');
-        }
-          
-        break
-      case "quote":
-      case "bulleted_list_item":
-        // console.log(block)
-        text = block[block.type].text
-        break
-      case "numbered_list_item":
-        text = block[block.type].text
-        break
-      // case "pdf":
-      //   //text = getMediaSourceText(block)
-      //   break
-      // case "equation":
-      //   text = block.equation.expression
-      //   break
-      // case "link_preview":
-      //   text = block.link_preview.url
-      //   break
-      // case "synced_block":
-      //   // Provides ID for block it's synced with.
-      //   text = block.synced_block.synced_from
-      //     ? "This block is synced with a block with the following ID: " +
-      //       block.synced_block.synced_from[block.synced_block.synced_from.type]
-      //     : "Source sync block that another blocked is synced with."
-      //   break
-      // case "table":
-      //   // Only contains table properties.
-      //   // Fetch children blocks for more details.
-      //   text = "Table width: " + block.table.table_width
-      //   break
-      // case "table_of_contents":
-      //   // Does not include text from ToC; just the color
-      //   text = "ToC color: " + block.table_of_contents.color
-      //   break
-      // case "breadcrumb":
-      // case "column_list":
-      // case "divider":
-      //   text = "No text available"
-      //   break
-      default:
-        console.log("Block type not recognized: " + block.type)
-        text = "[Needs case added]"
-        break
-    }
+    text = await getTextFromNonRichTextBlock(block);
   }
 
+  htmlTag = getHtmlTagByBlockType(block.type);
 
-  switch(block.type){
-    case "heading_1": return [["h2", text]];
-    case "heading_2": return [["h3", text]];
-    case "heading_3": return [["h4", text]];
-    case "paragraph": return [["p", text]];
-    case "bulleted_list_item": return [["li", text]];
-    case "numbered_list_item": return [["li", text]];
-    case "quote": return [["blockquote", text]];
-    case "image": return [["img", text]];
-    case "divider": return [["hr", text]];
-    default: {console.log(block.type + "  -  " + text); return ["p", text]};
-  }
-
+  return [[htmlTag, text]];
 }
 
-async function retrieveBlockChildren(id) {
-  console.log("Retrieving blocks (async)...")
-  const blocks = []
-
-  // Use iteratePaginatedAPI helper function to get all blocks first-level blocks on the page
-  for await (const block of iteratePaginatedAPI(notion.blocks.children.list, {
-    block_id: id, // A page ID can be passed as a block ID: https://developers.notion.com/docs/working-with-page-content#modeling-content-as-blocks
-  })) {
-    blocks.push(block)
-  }
-
-  return blocks
-}
-
-function getBlockById(id) {
-  return notion.blocks.retrieve({ block_id: id })
-}
-
-
-
-// const printBlockText = blocks => {
-//   console.log("Displaying blocks:")
-
-//   for (let i = 0; i < blocks.length; i++) {
-//     const text = getTextFromBlock(blocks[i])
-//     // Print plain text for each block.
-//     console.log(text)
-//   }
-// }
-
-async function changeBlocksForText(page, children, isToggle = false) {
-
-  let textBlocks = []
-  if(!isToggle)
-  {
-    textBlocks = [["h1", page.child_page.title]]
-  }else
-  {
-    textBlocks = [["h2", page.toggle.rich_text[0].plain_text]]
-  }
-  
-  for (let i = 0; i < children.length; i++) {
-    const texts = await getTextFromBlock(children[i])
-    for(let j = 0; j < texts.length; j++){
-      textBlocks.push(texts[j])
-    }
-  }
-  // console.log(textBlocks)
-  
-  return textBlocks
-}
-
-// function prepareTexts(texts){
-
-//   preparedTextList = []
-//   for(let i = 0; i < texts.length; i++){
-//     const text = texts[i]
-//     const splitIndex =  text.search(":")
-
-//     const splitTextArray = [text.slice(0, splitIndex), text.slice(splitIndex+1)]
-
-//     console.log(splitTextArray)
-//     preparedTextList.push(splitTextArray)
-//   }
-//   return preparedTextList
-// }
-
-
-// function makeHtmlFromTexts(texts) {
-
-//   const mainDiv = document.getElementById("test")
-//   textBlocks = []
-//   for (let i = 0; i < blocks.length; i++) {
-//     const text = getTextFromBlock(blocks[i])
-//     textBlocks.push(text)
-//     // Print plain text for each block.
-
-
-//     const newParagraphSuccessMsg = document.createElement("p")
-//     newParagraphSuccessMsg.textContent = text
-//     mainDiv.appendChild(newParagraphSuccessMsg)
-//   }
-  
-//   return textBlocks
-// }
-
-function chooseElementFromText(text){
-  switch(text){
-    case "heading_1": return "h1";
-    case "heading_2": return "h2";
-    case "heading_3": return "h3";
+function getHtmlTagByBlockType(blockType) {
+  switch (blockType) {
+    case "heading_1": return "h2";
+    case "heading_2": return "h3";
+    case "heading_3": return "h4";
     case "paragraph": return "p";
+    case "bulleted_list_item": return "li";
+    case "numbered_list_item": return "li";
+    case "quote": return "blockquote";
+    case "image": return "img";
+    case "divider": return "hr";
+    default: { console.log("Block type not recognized: " + blockType); return "p" };
   }
+}
+
+async function getTextFromRichTextBlock(block) {
+  text = getPlainTextFromRichText(block[block.type].rich_text)
+  if (text == undefined) {
+    errorPage = await getPageById(block.parent.page_id)
+    console.log("Page has not been shared with the integration - page title: " + errorPage.child_page.title)
+    text = ""
+  }
+  return text
+}
+
+async function getTextFromNonRichTextBlock(block) {
+  let text
+  switch (block.type) {
+    case "unsupported":
+      // The public API does not support all block types yet
+      text = "[Unsupported block type]"
+      break
+    case "paragraph":
+      text = block.paragraph.text
+      break
+    case "image":
+      const jsonString = JSON.stringify(block);
+      const urlMatch = jsonString.match(/"url":"(.*?)"/);
+
+      if (urlMatch && urlMatch[1]) {
+        const url = urlMatch[1];
+        text = url;
+      } else {
+        console.log('URL not found');
+      }
+
+      break
+    case "bulleted_list_item":
+      text = block[block.type].text
+      break
+    case "numbered_list_item":
+      text = block[block.type].text
+      break
+    case "divider":
+      text = ""
+      break
+  }
+
+  if (text == undefined) {
+    errorPage = await getPageById(block.parent.page_id)
+    console.log("type: " + block.type + " is unsupported, page title: " + errorPage.child_page.title)
+    text = ""
+  }
+
+  return text;
 }
 
 
